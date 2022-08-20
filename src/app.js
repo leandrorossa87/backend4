aea210fb141d2519b1c15d937c5fa1e86aea6c86
leandrorossa87 <leandrorossa87@gmail.com>
2022-08-20 17:48:45 -0300

    registro de rutas con prefijo

--- src/app.js
+++ src/app.js
@@ -6,7 +6,15 @@ const app = express();
 
 //de esta forma ingresa a la carpeta public en el buscargor ponemos
 //directamente el archivo sin nombrar la carpeta
-app.use(express.static('public'))  
+app.use(express.static('public'));  
+
+
+//busqueda de rutas con prefijo
+app.use('/busqueda', express.static('publica'));
+
+app.use('/celular', express.static('mobile'));
+
+app.use('/escritorio', express.static('desktop'));
 
 app.get('/', (req, res) => {
     res.send('Bienvenidos a la Pagina Principal')
