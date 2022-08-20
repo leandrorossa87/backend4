84cf6f56a700539bcde2be6d4308e6f9b194f2f2
leandrorossa87 <leandrorossa87@gmail.com>
2022-08-20 17:25:46 -0300

    creacion de carpeta public

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ const path = require('path')
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Bienvenidos')
-})
 
-app.listen(3000, () => console.log('Proyecto en el puerto 300'))
+//de esta forma ingresa a la carpeta public en el buscargor ponemos
+//directamente el archivo sin nombrar la carpeta
+app.use(express.static('public'))  
+
+app.get('/', (req, res) => {
+    res.send('Bienvenidos a la Pagina Principal')
+});
+
+
+app.listen(3000, ()=> console.log("Proyecto en el puerto 3000"));
